d0d5d719e613788680ea1ec650fff71153bed0d2
Blackbeing <jonnieey55@gmail.com>
2022-09-12 17:13:59 +0000

    feat Print alphabet in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lower and upper case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@
  - 0-positive_or_negative.c -- Print if number is positive, negative or zero
  - 1-last_digit -- print last digit
  - 2-print_alphabet -- print alphabet in lower case
+ - 3-print_alphabets -- print alphabet in lower and upper case
