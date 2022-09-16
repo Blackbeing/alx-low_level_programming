302860374c1a8afd089c4f5864c065b3a3ff2fca
Blackbeing <jonnieey55@gmail.com>
2022-09-16 23:18:49 +0000

    feat Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers
+ * @m: argument 1
+ * @n: argument 2
+ *
+ * Return: int
+ */
+
+
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,4 @@
  - 6-abs.c -- Absolute values
  - 7-print_last_digit.c -- print last digit of a number
  - 8-24_hours.c -- print minutes of the day
+ - 10-add -- Add two numbers
