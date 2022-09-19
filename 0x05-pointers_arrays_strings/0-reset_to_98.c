f92e7be4d0ae99ad1f5afeb64d636b2a450b7a77
Blackbeing <jonnieey55@gmail.com>
2022-09-19 21:44:19 +0000

    feat Reset number to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update integer value to 98
+ * @n: pointer to an int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Pointers, Arrays and Strings
+- 0-reset_to_98.c -- Reset number to 98
