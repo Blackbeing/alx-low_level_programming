a2cf5e0d7762caf73396ae0ca70f584f6323f56b
Blackbeing <jonnieey55@gmail.com>
2022-11-18 13:32:03 +0000

    refactor Free listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp_node;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		temp_node = (*head)->next;
 		free(*head);
 		*head = temp_node;
 	}
-	*head = NULL;
 }
