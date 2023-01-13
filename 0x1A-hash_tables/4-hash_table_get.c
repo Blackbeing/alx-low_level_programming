cb329def44e673cec2f90d98877a45453d68277e
Blackbeing <jonnieey55@gmail.com>
2023-01-13 23:20:17 +0000

    refactor Correction 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,10 +11,24 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int key_idx = 0;
 	const unsigned char *temp_key = (const unsigned char *)key;
+	hash_node_t *head;
+	int i = 0;
+
+	if (!ht || ht == NULL)
+		return (NULL);
 
 	key_idx = key_index(temp_key, ht->size);
 
 	if (!ht->array[key_idx])
 		return (NULL);
-	return (ht->array[key_idx]->value);
+
+	/* return (ht->array[key_idx]->key); */
+	head = ht->array[key_idx];
+	while (head)
+	{
+		if (strcmp(head[i].key,  key) == 0)
+			break;
+		head = head->next;
+	}
+	return (head->value);
 }
