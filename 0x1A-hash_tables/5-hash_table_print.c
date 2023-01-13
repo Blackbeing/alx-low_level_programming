f9d8b7f5a520dc08d1b97efc001812f1929cf59b
Blackbeing <jonnieey55@gmail.com>
2023-01-13 23:09:16 +0000

    refactor Correction 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -20,28 +20,25 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0;
 	unsigned long int count = 0;
 
-	if (!ht)
-		puts("");
-	else
+	if (!ht || ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("{");
-		for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
 		{
-			if (ht->array[i])
+			if (count > 0)
+				printf(", ");
+			print_node(ht->array[i]);
+			count++;
+			while (ht->array[i]->next != NULL)
 			{
-				if (count > 0)
-					printf(", ");
-				print_node(ht->array[i]);
-				count++;
-				while (ht->array[i]->next != NULL)
-				{
-					printf(", ");
-					print_node(ht->array[i]->next);
-					ht->array[i] = ht->array[i]->next;
-				}
+				printf(", ");
+				print_node(ht->array[i]->next);
+				ht->array[i] = ht->array[i]->next;
 			}
 		}
-		printf("}");
 	}
+	printf("}");
 	printf("\n");
 }
